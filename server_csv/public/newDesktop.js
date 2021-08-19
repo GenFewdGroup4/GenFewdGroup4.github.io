655643272f0485e4da41f25e5c1abfcc8c85962b
afrojsan <chung86813@gmail.com>
2021-08-19 17:07:18 +0800

    chagne-desktop-UI

--- server_csv/public/newDesktop.js
+++ server_csv/public/newDesktop.js
@@ -1,6 +1,23 @@
-const dueDate = document.querySelector('#due-date');
-const name = document.querySelector('#name');
-const status = document.querySelector('#status');
-const description = document.querySelector('#description');
-const assignedTo = document.querySelector('#assigned-to')
-const submit = document.querySelector('#submit')
+const dataForm = document.querySelector('#desktop-text-area')
+
+dataForm.addEventListener('submit', async(event) => {
+    event.preventDefault();
+    const form = event.target
+    const dataObj = {
+        duedate: form.duedate.value,
+        name: form.name.value,
+        description: form.description.value,
+        assignedto: form.assignedTo.value,
+        status: form.status.value
+    }
+    console.log(dataObj)
+
+    const response = await fetch('http://localhost:8080/todolist', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(dataObj)
+
+    })
+    const data = await response.json()
+    console.log(data)
+})
