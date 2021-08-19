34b1b082f8940c3ec594a88708c53898603bfe1c
afrojsan <chung86813@gmail.com>
2021-08-19 16:48:01 +0800

    added server

---
+++ server_csv/public/newDesktop.js
@@ -0,0 +1,6 @@
+const dueDate = document.querySelector('#due-date');
+const name = document.querySelector('#name');
+const status = document.querySelector('#status');
+const description = document.querySelector('#description');
+const assignedTo = document.querySelector('#assigned-to')
+const submit = document.querySelector('#submit')
