0eeddd46881523733b8098ab24eba711efa7461e
kbawto <kbawto@gmail.com>
2021-09-09 09:27:20 +0800

    add cors

--- server_csv/main.ts
+++ server_csv/main.ts
@@ -1,30 +1,30 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import { createRoute } from './routes'
-import { Controller } from './controller'
-import { Service } from './service';
-import { UserService } from './UserService';
-import { UserController } from './UserController';
-import { createUserRoute } from './userRoutes';
+import express from "express";
+import bodyParser from "body-parser";
+import { createRoute } from "./routes";
+import { Controller } from "./controller";
+import { Service } from "./service";
+import { UserService } from "./UserService";
+import { UserController } from "./UserController";
+import { createUserRoute } from "./userRoutes";
 
 const app = express();
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const service = new Service();
 const controller = new Controller(service);
 const routes = createRoute(controller);
 
 const userService = new UserService();
-const userController = new UserController(userService)
-const userRoutes = createUserRoute(userController)
+const userController = new UserController(userService);
+const userRoutes = createUserRoute(userController);
 
-app.use('/', express.static('./public'));
+app.use("/", express.static("./public"));
 app.use(routes);
-app.use('/user',userRoutes)
+app.use("/user", userRoutes);
 
-const PORT = 8080
+const PORT = 8080;
 app.listen(PORT, () => {
-    console.log('Listening on port: ' + PORT)
-    console.log(`Please visit: http://localhost:${PORT}`)
-})
+  console.log("Listening on port: " + PORT);
+  console.log(`Please visit: http://localhost:${PORT}`);
+});
